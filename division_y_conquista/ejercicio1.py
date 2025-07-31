07e7b5a15f12efc4c9e25ec558045b38fa352a5a
tinchomorilla <mmorilla@fi.uba.ar>
2025-07-31 20:21:04 -0300

    initial commit

---
+++ division_y_conquista/ejercicio1.py
@@ -0,0 +1,3 @@
+# (★) Implementar, por división y conquista, una función que dado un arreglo 
+#sin elementos repetidos y casi ordenado (todos los elementos se encuentran ordenados, salvo uno), 
+#obtenga el elemento fuera de lugar. Indicar y justificar su complejidad temporal.
