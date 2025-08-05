68d65a01c11d7966cd5a0f4654f6bb48ee14626b
tinchomorilla <mmorilla@fi.uba.ar>
2025-08-05 00:20:00 -0300

    statement

---
+++ division_y_conquista/ejercicio3.py
@@ -0,0 +1,4 @@
+# (★) Implementar un algoritmo que, por división y conquista, permita obtener 
+# la parte entera de la raíz cuadrada de 
+# un número n, en tiempo O(logn). Por ejemplo, para n=10 debe devolver 3, 
+# y para n=25 debe devolver 5. Justificar la complejidad del algoritmo.
