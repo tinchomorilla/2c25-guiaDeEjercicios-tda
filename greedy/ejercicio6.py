566d089778ffd93db12bc7dfc6560088ae2a21e4
tinchomorilla <mmorilla@fi.uba.ar>
2025-08-10 23:27:49 -0300

    statement

---
+++ greedy/ejercicio6.py
@@ -0,0 +1,9 @@
+# (★) Se tiene un sistema monetario (ejemplo, el nuestro). 
+# Se quiere dar “cambio” de una determinada cantidad de plata. 
+# Implementar un algoritmo Greedy que devuelva el cambio pedido, usando la mínima cantidad de monedas/billetes. 
+# El algoritmo recibirá un arreglo de valores del sistema monetario, 
+# y la cantidad de cambio objetivo a dar, y debe devolver qué monedas/billetes deben 
+# ser utilizados para minimizar la cantidad total utilizda. 
+# Indicar y justificar la complejidad del algoritmo implementado. 
+# ¿El algoritmo implementado encuentra siempre la solución óptima? 
+# Justificar si es óptimo, o dar un contraejemplo. ¿Por qué se trata de un algoritmo Greedy? Justificar
