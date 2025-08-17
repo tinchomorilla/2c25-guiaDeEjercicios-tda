4688bd8dd02635a98fdade38c45118555f252ef8
tinchomorilla <mmorilla@fi.uba.ar>
2025-08-16 21:29:13 -0300

    statement

---
+++ greedy/ejercicio16.py
@@ -0,0 +1,10 @@
+# (★★★) El club de Amigos de Siempre prepara una cena en sus instalaciones 
+# en la que desea invitar a la máxima cantidad de sus n socios. 
+# Sin embargo por protocolo cada persona invitada debe cumplir un requisito: 
+# Sólo puede ser invitada si conoce a al menos otras 4 personas invitadas. 
+# a. Nos solicitan seleccionar el mayor número posible de invitados. 
+# Proponer una estrategia greedy óptima para resolver el problema.
+# b. Los organizadores desean que cada invitado pueda conocer nuevas personas. 
+# Por lo que nos solicitan que adicionemos una nueva restricción a la invitación: 
+# Sólo puede asistir si NO conoce al menos otras 4 personas invitadas. 
+# Modifique su propuesta para satisfacer esta nueva solución.
