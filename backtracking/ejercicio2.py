1ad0b971c93db1c9f9145275b5e4dad6659cfd9f
tinchomorilla <mmorilla@fi.uba.ar>
2025-08-18 16:06:00 -0300

    statement

---
+++ backtracking/ejercicio2.py
@@ -0,0 +1,4 @@
+# (★★) Implementar un algoritmo que reciba un grafo y un número n que, 
+# utilizando backtracking, indique si es posible pintar cada vértice 
+# con n colores de tal forma que no hayan dos vértices adyacentes con el mismo color.
+
