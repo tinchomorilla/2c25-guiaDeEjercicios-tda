791add74857f9cc57239009bc2398675028604db
tinchomorilla <mmorilla@fi.uba.ar>
2025-08-29 16:08:46 -0300

    refactor to improve performance and clarity; implement caching and logging of results.

--- TP0/tp0.py
+++ TP0/tp0.py
@@ -1,66 +1,55 @@
 import time
 
-# def amigos(MAX):
-#     t1=time.time()
-#     for i in range(MAX):
-#         s=0
-#         for j in range (1,i-1+1):
-#             if i%j==0:
-#                 s+=j
-#         s2=0
-#         for k in range (1,s-1):
-#             if s%k==0:
-#                 s2+=k
-#         if i==s2:
-#             print(i,s)
-#     t2=time.time()
-#     print(t2-t1)
-
 
 def amigos(MAX):
-    amigos = set()
-    prints = []
-    t1=time.time()
-    for i in range(MAX):
-        if i not in amigos:
-            posible_amigo = suma_de_divisores(i)
+    cache = {}
+    prints = [(0,0)]  # caso base 
+    t1 = time.time()
+    
+    for i in range(2, MAX+1):  # arranco en 2 hasta MAX
+        if i not in cache:
+            cache[i] = suma_de_divisores(i)
+            posible_amigo = cache[i]
+
+            # caso numero perfecto
             if posible_amigo == i:
-                prints.append((i,i))
-                amigos.add(i)
-            else:
-                result = suma_de_divisores(posible_amigo)
-                if result == i:
-                    amigos.add(i)
-                    amigos.add(posible_amigo)
-                    prints.append((i,posible_amigo))
-    t2=time.time()
-    print(t2-t1)
-    for p in prints:
-        print(p)
+                prints.append((i, i))
+                continue
+
+            # chequeamos simetria
+            if posible_amigo not in cache:
+                cache[posible_amigo] = suma_de_divisores(posible_amigo)
+
+            # condicion de amistad
+            if (posible_amigo in cache and cache[posible_amigo] == i):
+                prints.append((i, posible_amigo))
+
+    t2 = time.time()
+    log_prints(prints, t2 - t1)
 
 
 def suma_de_divisores(n):
-    if n <= 1:
-        return 0
-    
     # Iterar hasta sqrt(n)
-    sum = 0
-    for i in range(1, int(n**0.5) + 1):
+    sum = 1
+    for i in range(2, int(n**0.5) + 1):
         if n % i == 0:
             if i != n:  
                 sum += i
             if n // i != n:  
                 sum += n//i
-    
-    
     return sum
+
+
+def log_prints(prints, elapsed_time):
+    for p in prints:
+        print(p)
+    print("Cantidad de pares amigos:", len(prints))
+    print("El tiempo es de:", elapsed_time)
     
 
 def main():
 
-    
     amigos(1000000)
-
 
 if __name__ == "__main__":
     main()
