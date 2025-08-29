995a27b7ffc90f9aa7541598644767dbcb8e7406
tinchomorilla <mmorilla@fi.uba.ar>
2025-08-29 14:50:54 -0300

    store results (prints) in a list and improve perfomance

--- TP0/tp0.py
+++ TP0/tp0.py
@@ -1,3 +1,4 @@
+import time
 
 # def amigos(MAX):
 #     t1=time.time()
@@ -18,19 +19,24 @@
 
 def amigos(MAX):
     amigos = set()
+    prints = []
+    t1=time.time()
     for i in range(MAX):
         if i not in amigos:
             posible_amigo = suma_de_divisores(i)
             if posible_amigo == i:
-                print(i,i)
+                prints.append((i,i))
                 amigos.add(i)
             else:
                 result = suma_de_divisores(posible_amigo)
                 if result == i:
                     amigos.add(i)
                     amigos.add(posible_amigo)
-                    print(i,posible_amigo)
-
+                    prints.append((i,posible_amigo))
+    t2=time.time()
+    print(t2-t1)
+    for p in prints:
+        print(p)
 
 
 def suma_de_divisores(n):
@@ -45,6 +51,7 @@ def suma_de_divisores(n):
                 sum += i
             if n // i != n:  
                 sum += n//i
+    
     
     return sum
     
